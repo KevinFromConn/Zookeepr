37d85ae2d3c5343aa301ae3f0ea82b89c2d65256
KevinFromConn <kfc21194@gmail.com>
2021-07-26 14:10:30 -0400

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { animals } = require('./data/animals');
 
 const express = require('express');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 47314;
 
 const app = express();
 
